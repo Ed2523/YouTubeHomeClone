6bf091b4293376d516f05b425cbb57d9ff6f2c02
Edgar Chavez <citylight2392@gmail.com>
2024-07-17 11:24:31 -0600

    creation of components folder and button component

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Menu } from "lucide-react";
 import youTubeLogo from "../assets/youtubelogo.png";
 
 const PageHeader = () => {
   return (
     <div className='flex gap-10 lg:gap-20 justify-between'>
       <div className='flex gap-4 items-center flex-shrink-0'>
-        <button></button>
+        <button>
+          <Menu />
+        </button>
         <a href='/'>
           {" "}
           <img src={youTubeLogo} className='h-6' />
