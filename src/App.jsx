c8be927b0746d8d5474c2fa92cd2ed789b88ee96
Edgar Chavez <citylight2392@gmail.com>
2024-07-17 11:12:55 -0600

    setting react typescript app with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PageHeader from "./layouts/PageHeader";
+
+const App = () => {
+  return (
+    <div className='max-h-screen flex flex-col'>
+      <PageHeader />
+      <div>2</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/layouts/PageHeader.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import youTubeLogo from "../assets/youtubelogo.png";
+
+const PageHeader = () => {
+  return (
+    <div className='flex gap-10 lg:gap-20 justify-between'>
+      <div className='flex gap-4 items-center flex-shrink-0'>
+        <button></button>
+        <a href='/'>
+          {" "}
+          <img src={youTubeLogo} className='h-6' />
+        </a>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default PageHeader;
